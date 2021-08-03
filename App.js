e1afcc7608601304655b1ec17c50488179ac9458
Rafleone <rafik.pilumyan@gmail.com>
2021-08-03 19:32:43 +0300

    Created dummy data and used props for show and get different data to show

--- App.js
+++ App.js
@@ -3,12 +3,16 @@ import {SafeAreaView} from 'react-native';
 
 import HomeScreen from './src/screens/Home';
 import Post from './src/components/Post';
+import feed from './assets/data/feed';
+
+const post1 = feed[0];
+const post2 = feed[1];
 
 const App = () => {
   return (
     <SafeAreaView>
       {/* <HomeScreen /> */}
-      <Post />
+      <Post post={post2} />
     </SafeAreaView>
   );
 };
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -2,36 +2,38 @@ import React from 'react';
 import {View, Image, Text} from 'react-native';
 import styles from './styles';
 
-const Post = () => {
+const Post = props => {
+  const post = props.post;
+
   return (
     <View style={styles.container}>
       {/* Image */}
       <Image
         style={styles.image}
         source={{
-          uri: 'https://assets.bedful.com/images/21896bfd778e98fb84b1e69dbd77bad9c9107714/large.jpg',
+          uri: post.image,
         }}
       />
 
       {/* Bed & Bedroom */}
-      <Text style={styles.bedrooms}>1 bed 1 bedroom</Text>
+      <Text style={styles.bedrooms}>
+        {post.bed} bed {post.bedroom} bedroom
+      </Text>
 
       {/* Type & Description */}
       <Text style={styles.description} numberOfLines={2}>
-        There aren't too many places in Britain where you can squint and pretend
-        you're in the Caribbean for the moment, But on a sunny day the Buddha
-        Beach House is one of them.
+        {post.type}. {post.title}
       </Text>
 
       {/* Old price & new Price */}
       <Text style={styles.prices}>
-        <Text style={styles.oldPrice}>$36</Text>
-        <Text style={styles.newPrice}> $30</Text>
-        <Text style={styles.nightText}>/ night</Text>
+        <Text style={styles.oldPrice}>${post.oldPrice}</Text>
+        <Text style={styles.newPrice}> ${post.newPrice}</Text>
+        <Text style={styles.nightText}> / night</Text>
       </Text>
 
       {/* Total price */}
-      <Text style={styles.totalPrice}>$230 total</Text>
+      <Text style={styles.totalPrice}>${post.totalPrice} total</Text>
     </View>
   );
 };
