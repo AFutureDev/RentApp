dd23d9442d9fbb410c2995b1efe0de8f5f13f222
Rafleone <rafik.pilumyan@gmail.com>
2021-08-03 14:55:08 +0300

    Created React Native CLI Project and installed vector icons

---
+++ src/screens/Home/index.js
@@ -0,0 +1,14 @@
+/* eslint-disable prettier/prettier */
+import React from 'react'
+import { Text, View } from 'react-native'
+import styles from './style';
+
+const HomeScreen = (props) => {
+    return (
+        <View>
+            <Text>Raf</Text>
+        </View>
+    )
+}
+
+export default HomeScreen;
